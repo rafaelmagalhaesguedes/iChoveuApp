76be7ed234df3c8976ada898038268daa5acbea9
Rafael <rafaelmagalhaes1@live.com>
2023-08-12 11:07:01 -0300

    fix: change http to https

--- src/services/api.js
+++ src/services/api.js
@@ -8,7 +8,7 @@ const FORECAST = 7; // Total de dias para previsão
 * Search Cities by term
 */
 export const searchCities = async (term) => {
-  const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${APIKEY}&q=${term}`);
+  const response = await fetch(`https://api.weatherapi.com/v1/search.json?lang=pt&key=${APIKEY}&q=${term}`);
   const data = await response.json();
   if (data.length === 0) {
     alert('Nenhuma cidade encontrada');
@@ -21,7 +21,7 @@ export const searchCities = async (term) => {
 * Get Weather by city
 */
 export const getWeatherByCity = async (url) => {
-  const response = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${APIKEY}&q=${url}`);
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?lang=pt&key=${APIKEY}&q=${url}`);
   const data = await response.json();
   const weatherInfo = {
     temp: data.current.temp_c,
@@ -35,7 +35,7 @@ export const getWeatherByCity = async (url) => {
 * Get Infos City Weather 7 days
 */
 export const fetchForecastData = async (cityUrl) => {
-  const urlApi = `http://api.weatherapi.com/v1/forecast.json?lang=pt&key=${APIKEY}&q=${cityUrl}&days=${FORECAST}`;
+  const urlApi = `https://api.weatherapi.com/v1/forecast.json?lang=pt&key=${APIKEY}&q=${cityUrl}&days=${FORECAST}`;
   const response = await fetch(urlApi);
   const data = await response.json();
   if (data.length === 0) throw new Error('Error API connect');
